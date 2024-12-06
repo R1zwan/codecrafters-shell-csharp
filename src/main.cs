2c426986bcb40388de572833feead86ffdaeaa13
R1zwan <mohammedrizwan.afgani@gmail.com>
2024-12-06 20:20:52 +0530

    repl

--- src/main.cs
+++ src/main.cs
@@ -2,8 +2,9 @@ using System.Net;
 using System.Net.Sockets;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // Console.WriteLine("Logs from your program will appear here!");
-
-Console.Write("$ ");
-
-var command = Console.ReadLine();
-Console.WriteLine($"{command}: command not found");
+while (true)
+{
+    Console.Write("$ ");
+    var command = Console.ReadLine();
+    Console.WriteLine($"{command}: command not found");
+}
