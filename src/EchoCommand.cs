a32f1ab183a128cac9ac527e9394d66277924ff4
R1zwan <mohammedrizwan.afgani@gmail.com>
2024-12-25 16:47:06 +0530

    debug

--- src/EchoCommand.cs
+++ src/EchoCommand.cs
@@ -2,7 +2,7 @@ public class EchoCommand : ICommand
 {
     public void Execute(string[] arguments)
     {
-        Console.WriteLine($"test args - {arguments}");
+        Console.WriteLine($"test args - {arguments.Length}");
         Console.WriteLine(string.Join(" ", arguments));
     }
 }
