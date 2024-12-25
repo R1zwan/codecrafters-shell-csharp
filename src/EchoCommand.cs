db25226385ec3cc20466fb9d68c0e937ac74d851
R1zwan <mohammedrizwan.afgani@gmail.com>
2024-12-25 16:44:39 +0530

    debug echo

--- src/EchoCommand.cs
+++ src/EchoCommand.cs
@@ -2,6 +2,7 @@ public class EchoCommand : ICommand
 {
     public void Execute(string[] arguments)
     {
+        Console.WriteLine($"test args - {arguments}");
         Console.WriteLine(string.Join(" ", arguments));
     }
 }
